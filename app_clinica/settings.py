16afb651a45d85be0a73afbdc6c023c442f1ba72
unknown <dayvidallan@hotmail.com>
2022-05-04 16:13:12 -0300

    atualizando as templates index

--- app_clinica/settings.py
+++ app_clinica/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #APP INSTALADOS
+    'fontawesomefree',
     #MEUS APPS
     'clinica',
     'funcionarios',
