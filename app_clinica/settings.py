02835814986c30a15e828b5950875b3202269c2f
unknown <dayvidallan@hotmail.com>
2022-05-05 17:55:46 -0300

    Configuring the app

--- app_clinica/settings.py
+++ app_clinica/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['app-clinica-odonto.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['app-odonto-clinica.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
