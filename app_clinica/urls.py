2a2e9a14686aff0eba4035058978c0bdf2329143
unknown <dayvidallan@hotmail.com>
2022-05-05 17:47:00 -0300

    Configuring the app

--- app_clinica/urls.py
+++ app_clinica/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('clinica.urls')),
@@ -11,4 +14,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
