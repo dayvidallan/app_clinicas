bfd98fc8d08049da26984ed2fb42780b3a21885c
Easy Gestão <dayvidallan@hotmail.com>
2022-05-14 13:47:34 -0300

    resolvendo os uploads2

--- clientes/models.py
+++ clientes/models.py
@@ -13,7 +13,7 @@ class Paciente(models.Model):
     telefone = models.CharField(max_length=100, unique=False, verbose_name='Nº telefone celular')
     endereco = models.CharField(max_length=100, unique=False)
     data_consulta = models.DateTimeField(default=timezone.now)
-    anamnesi = models.TextField(null=True, blank=True)
+    anamnesi = models.TextField(null=True, blank=True, verbose_name='Ananimese')
     upload = models.FileField(upload_to='perfil', null=True, blank=True)
 
     class Meta:
@@ -45,7 +45,7 @@ class Procedimento(models.Model):
 class Consulta(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     data_consulta = models.DateTimeField(default=timezone.now)
-    anamnesi = models.CharField(max_length=1000, null=True, blank=True)
+    anamnesi = models.CharField(max_length=1000, null=True, blank=True, verbose_name='Ananimese')
     upload = models.FileField(upload_to='upload', null=True, blank=True, verbose_name='Documentos')
 
     class Meta:
